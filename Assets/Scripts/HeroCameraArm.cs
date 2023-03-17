840a4075b9179824ced295153fccf3b5a78b1dac
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2023-03-18 00:18:14 +0200

    Push camera out of solids

--- Assets/Scripts/HeroCameraArm.cs
+++ Assets/Scripts/HeroCameraArm.cs
@@ -5,15 +5,23 @@ using UnityEngine.InputSystem;
 public class HeroCameraArm : MonoBehaviour
 {
     [SerializeField] private ScrapballContents _ball;
+    [SerializeField] private LayerMask _pushOutLayers;
+    [SerializeField] private float _pushOutDistance = 0.5f;
+    [SerializeField] private float _pushOutFreeRadius = 0.5f;
     [SerializeField] private float _camSensitivity = 0.5f;
     [SerializeField] private float _camDistanceBase = 1.5f;
     [SerializeField] private float _camDistanceScale = 0.5f;
 
     public Vector2 CameraRotation = new Vector2();
 
+    private float _camDestinationDistance = 1f;
+    private float _raycastSourceDistance = 1f;
+    private Vector3 _camDirection;
+
+
     public void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void OnLook(InputAction.CallbackContext context)
@@ -21,16 +29,36 @@ public class HeroCameraArm : MonoBehaviour
         Vector2 delta = context.ReadValue<Vector2>();
         CameraRotation += delta * _camSensitivity;
         CameraRotation.y = Mathf.Clamp(CameraRotation.y, -85f, 85f);
-        transform.eulerAngles = new Vector3(-CameraRotation.y, CameraRotation.x, 0f);
+        _camDirection = Quaternion.Euler(-CameraRotation.y, CameraRotation.x, 0f) * Vector3.back;
     }
 
     public void OnBallRadiusChanged(float to)
     {
-        transform.localScale = (to * _camDistanceScale + _camDistanceBase) * Vector3.one;
+        _camDestinationDistance = to * _camDistanceScale + _camDistanceBase;
+        _raycastSourceDistance = to + 1f;
     }
 
     public void OnValidate()
     {
         OnBallRadiusChanged(_ball.ScrapRadius);
+        _camDirection = Quaternion.Euler(-CameraRotation.y, CameraRotation.x, 0f) * Vector3.back;
+    }
+
+    private void Update()
+    {
+        if (Physics.SphereCast(
+            _ball.transform.position + _camDirection * _raycastSourceDistance,
+            _pushOutFreeRadius,
+            _camDirection,
+            out var hitInfo,
+            _camDestinationDistance - _raycastSourceDistance + _pushOutDistance,
+            _pushOutLayers))
+        {
+            transform.position = hitInfo.point - _camDirection * _pushOutDistance;
+        }
+        else
+        {
+            transform.position = _ball.transform.position + _camDirection * _camDestinationDistance;
+        }
     }
 }
